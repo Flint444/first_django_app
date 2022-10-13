a84fe28d1cb171781c37b0067eb3d4e8eea0577b
Flint444 <maksimgrabauskas@gmail.com>
2022-10-13 13:38:57 +0200

    Third version #Добавлены редактирование и удаление записей

--- kr/news/urls.py
+++ kr/news/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.news_home, name = 'news_home'),
     path('create', views.create, name = 'create'),
     path('<int:pk>', views.NewsDetailVies.as_view(), name = 'news-detail'),
+    path('<int:pk>/update', views.NewsUpdateVies.as_view(), name = 'news-update'),
+    path('<int:pk>/delete', views.NewsDeleteVies.as_view(), name = 'news-delete'),
 ]
--- kr/news/views.py
+++ kr/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 # Create your views here.
 
 def news_home(request):
@@ -12,6 +12,17 @@ class NewsDetailVies(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+class NewsUpdateVies(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+class NewsDeleteVies(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
 
 def create(request):
     error = ''
