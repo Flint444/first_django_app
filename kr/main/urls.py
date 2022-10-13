ccb837b402ef4304e1edc501f205a8c20ccd4be4
Flint444 <maksimgrabauskas@gmail.com>
2022-10-13 20:52:30 +0200

    7.1th version #Кнопки редактировать и удалить доступны только авторизованным пользователям

--- kr/main/urls.py
+++ kr/main/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('contacts', views.contacts, name = 'contacts'),
     path('signup', views.SignupView.as_view(), name = 'signup'),
     path('login', views.MyloginView.as_view(), name = 'login'),
-    path('logout', LogoutView.as_view(next_page = 'login'), name = 'logout'),
+    path('logout', LogoutView.as_view(next_page = 'home'), name = 'logout'),
 ]
